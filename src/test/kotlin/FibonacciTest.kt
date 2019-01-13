8b96ec9f0a1346ceedcbfa4b33719dd5dfcef48b
ldavidgomez <david.gomez@apiumhub.com>
2019-01-13 12:23:02 +0100

    refactor to parameterized test

--- src/test/kotlin/FibonacciTest.kt
+++ src/test/kotlin/FibonacciTest.kt
@@ -1,47 +1,28 @@
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class FibonacciTest {
 
     private val fibonacci = Fibonacci()
 
-    @Test
-    fun should_return_0_for_position_0() {
-        val expected = 0
-        val result = fibonacci.callForAction(0)
+    @ParameterizedTest(name = "Position {0} should be fibonacci number {1}")
+    @CsvSource(
+        "0, 0",
+        "1, 1",
+        "2, 1,",
+        "3, 2",
+        "4, 3",
+        "5, 5",
+        "6, 8",
+        "7, 13",
+        "8, 21",
+        "9, 34"
+    )
+    fun should_return_fibonazzi_number_for_position(position: Int, expected: Int) {
+        val result = fibonacci.callForAction(position)
 
         assertEquals(expected, result)
     }
 
-    @Test
-    fun should_return_1_for_position_1() {
-        val expected = 1
-        val result = fibonacci.callForAction(1)
-
-        assertEquals(expected, result)
-    }
-
-    @Test
-    fun should_return_1_for_position_2() {
-        val expected = 1
-        val result = fibonacci.callForAction(2)
-
-        assertEquals(expected, result)
-    }
-
-    @Test
-    fun should_return_2_for_position_3() {
-        val expected = 2
-        val result = fibonacci.callForAction(3)
-
-        assertEquals(expected, result)
-    }
-
-    @Test
-    fun should_return_21_for_position_8() {
-        val expected = 21
-        val result = fibonacci.callForAction(8)
-
-        assertEquals(expected, result)
-    }
 }
