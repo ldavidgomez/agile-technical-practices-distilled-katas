b98b6657c22103e36f3d7d6ad0eb8acd4ee9d1a8
ldavidgomez <david.gomez@apiumhub.com>
2019-01-13 10:48:52 +0100

    refactor

--- src/test/kotlin/ThingTest.kt
+++ src/test/kotlin/ThingTest.kt
@@ -7,30 +7,6 @@ class ThingTest {
 
     private val thing: Thing = Thing()
 
-    @Test
-    fun should_return_1() {
-        val expected = "1"
-        val result = thing.callForAction(1)
-
-        assertEquals(result, expected)
-    }
-
-    @Test
-    fun should_return_2() {
-        val expected = "2"
-        val result = thing.callForAction(2)
-
-        assertEquals(result, expected)
-    }
-
-    @Test
-    fun should_return_4() {
-        val expected = "4"
-        val result = thing.callForAction(4)
-
-        assertEquals(result, expected)
-    }
-
     @ParameterizedTest(name = "String of {0} should be {1}")
     @CsvSource("1, 1", "2, 2", "4, 4")
     fun should_return_non_fizzbuzz_string (number: Int, expected: String) {
