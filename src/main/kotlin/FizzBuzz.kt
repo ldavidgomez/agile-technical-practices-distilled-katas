ee543c51c9393cbf3b4c59a9f01aef477a29e703
ldavidgomez <david.gomez@apiumhub.com>
2019-01-13 11:23:03 +0100

    refactor

--- src/main/kotlin/FizzBuzz.kt
+++ src/main/kotlin/FizzBuzz.kt
@@ -1,4 +1,4 @@
-class Thing {
+class FizzBuzz {
     fun callForAction(number: Int): String {
         if(number.rem(3) == 0 && number.rem(5) == 0) return "FizzBuzz"
         if(number.rem(3) == 0) return "Fizz"
--- src/test/kotlin/FizzBuzzTest.kt
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -2,14 +2,14 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
-class ThingTest {
+class FizzBuzzTest {
 
-    private val thing: Thing = Thing()
+    private val fizzBuzz: FizzBuzz = FizzBuzz()
 
     @ParameterizedTest(name = "String of {0} should be {1}")
     @CsvSource("1, 1", "2, 2", "4, 4")
     fun should_return_non_fizzbuzz_string (number: Int, expected: String) {
-        val result = thing.callForAction(number)
+        val result = fizzBuzz.callForAction(number)
 
         assertEquals(expected, result)
     }
@@ -18,7 +18,7 @@ class ThingTest {
     @CsvSource("3", "6", "9")
     fun should_return_fizz_string(number: Int) {
         val expected = "Fizz"
-        val result = thing.callForAction(number)
+        val result = fizzBuzz.callForAction(number)
 
         assertEquals(expected, result)
     }
@@ -27,7 +27,7 @@ class ThingTest {
     @CsvSource("5", "10", "20")
     fun should_return_buzz_string(number: Int) {
         val expected = "Buzz"
-        val result = thing.callForAction(number)
+        val result = fizzBuzz.callForAction(number)
 
         assertEquals(expected, result)
     }
@@ -36,7 +36,7 @@ class ThingTest {
     @CsvSource("15", "30", "45")
     fun should_return_fizzbuzz_string(number: Int) {
         val expected = "FizzBuzz"
-        val result = thing.callForAction(number)
+        val result = fizzBuzz.callForAction(number)
 
         assertEquals(expected, result)
     }
