194df5532ec4d22e3bb72a53f6c45e264aaca4e2
ldavidgomez <david.gomez@apiumhub.com>
2019-01-13 12:18:32 +0100

    refactor

--- src/main/kotlin/Fibonacci.kt
+++ src/main/kotlin/Fibonacci.kt
@@ -5,7 +5,7 @@ class Fibonacci {
 
         if(position == 0) return 0
         if(position == 1) return 1
-        println("${position} + ${position - 1}")
+
         return callForAction(position - 1) + callForAction(position - 2)
     }
 
