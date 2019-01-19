ec6886f13be433df58e5ec176faae5046c753333
ldavidgomez <david.gomez@apiumhub.com>
2019-01-19 13:31:07 +0100

    Add parametized test for units

--- src/test/kotlin/FibonacciTest.kt
+++ src/test/kotlin/FibonacciTest.kt
@@ -8,16 +8,16 @@ class FibonacciTest {
 
     @ParameterizedTest(name = "Position {0} should be fibonacci number {1}")
     @CsvSource(
-        "0, 0",
-        "1, 1",
-        "2, 1,",
-        "3, 2",
-        "4, 3",
-        "5, 5",
-        "6, 8",
-        "7, 13",
-        "8, 21",
-        "9, 34"
+            "0, 0",
+            "1, 1",
+            "2, 1,",
+            "3, 2",
+            "4, 3",
+            "5, 5",
+            "6, 8",
+            "7, 13",
+            "8, 21",
+            "9, 34"
     )
     fun should_return_fibonazzi_number_for_position(position: Int, expected: Int) {
         val result = fibonacci.callForAction(position)
--- src/test/kotlin/RomanNumeralsTest.kt
+++ src/test/kotlin/RomanNumeralsTest.kt
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class RomanNumeralsTest {
 
@@ -65,5 +67,23 @@ class RomanNumeralsTest {
         assertEquals("V", result)
     }
 
+    @ParameterizedTest
+    @CsvSource(
+        "1, I",
+        "2, II,",
+        "3, III",
+        "4, IV",
+        "5, V",
+        "6, VI",
+        "7, VII",
+        "8, VIII",
+        "9, IX"
+    )
+    fun should_return_roman_unit_when_receive_arabic_unit(arabicNumber: Int, romanNumber: String) {
+        val result = romanNumerals.callForAction(arabicNumber)
+
+        //then
+        assertEquals(romanNumber, result)
+    }
 
 }
