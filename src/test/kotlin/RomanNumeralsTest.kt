c621c4d96c545c8f67b4fe61c0717b70590191c5
ldavidgomez <david.gomez@apiumhub.com>
2019-01-19 20:32:57 +0100

    refactor

--- src/test/kotlin/RomanNumeralsTest.kt
+++ src/test/kotlin/RomanNumeralsTest.kt
@@ -17,7 +17,18 @@ class RomanNumeralsTest {
         "6, VI",
         "7, VII",
         "8, VIII",
-        "9, IX"
+        "9, IX",
+        "10, X",
+        "11, XI",
+        "12, XII",
+        "13, XIII",
+        "14, XIV",
+        "15, XV",
+        "16, XVI",
+        "17, XVII",
+        "18, XVIII",
+        "19, XIX",
+        "20, XX"
     )
     fun should_return_roman_unit_when_receive_arabic_unit(arabicNumber: Int, romanNumber: String) {
         val result = romanNumerals.callForAction(arabicNumber)
