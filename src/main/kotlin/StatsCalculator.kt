2f0ce67b175116fa55c0767eabb0a01a0917bf08
ldavidgomez <david.gomez@apiumhub.com>
2019-01-19 12:26:41 +0100

    refactor

--- src/main/kotlin/StatsCalculator.kt
+++ src/main/kotlin/StatsCalculator.kt
@@ -1,9 +1,9 @@
 class StatsCalculator {
 
     fun callForAction(sequence: IntArray): Stats {
-        return Stats(sequence.min(), sequence.max(), 0,0 )
+        return Stats(sequence.min(), sequence.max(), sequence.size,sequence.average() )
     }
 
 }
 
-data class Stats(val min: Int? = 0, val max: Int? = 0, val size: Int? = 0, val average: Int? = 0)
+data class Stats(val min: Int? = 0, val max: Int? = 0, val size: Int? = 0, val average: Double = 0.0)
--- src/test/kotlin/StatsCalculatorTest.kt
+++ src/test/kotlin/StatsCalculatorTest.kt
@@ -1,23 +1,46 @@
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class StatsCalculatorTest {
 
     private val statsCalculator = StatsCalculator()
-    private val stats: Stats = Stats(0, 0,0,0)
+    private val stats: Stats = Stats(0, 0,0,0.0)
 
     @Test
     fun should_return_min_value_from_sequence() {
         val result = statsCalculator.callForAction(intArrayOf(6, 9, 15, -2, 92, 11))
 
-        assertEquals(Stats(-2, 0, 0, 0).min, result.min)
+        assertEquals(Stats(-2, 0, 0, 0.0).min, result.min)
     }
 
     @Test
     fun should_return_max_value_from_sequence() {
         val result = statsCalculator.callForAction(intArrayOf(6, 9, 15, -2, 92, 11))
 
-        assertEquals(Stats(0, 92, 0, 0).max, result.max)
+        assertEquals(Stats(0, 92, 0, 0.0).max, result.max)
+    }
+
+    @Test
+    fun should_return_size_value_from_sequence() {
+        val result = statsCalculator.callForAction(intArrayOf(6, 9, 15, -2, 92, 11))
+
+        assertEquals(Stats(0, 0, 6, 0.0).size, result.size)
+    }
+
+    @Test
+    fun should_return_average_value_from_sequence() {
+        val result = statsCalculator.callForAction(intArrayOf(6, 9, 15, -2, 92, 11))
+
+        assertEquals(Stats(0, 0, 0, 21.833333333333332).average, result.average)
+    }
+
+    @Test
+    fun should_return_complete_value_from_sequence() {
+        val result = statsCalculator.callForAction(intArrayOf(6, 9, 15, -2, 92, 11))
+
+        assertEquals(Stats(-2, 92, 6, 21.833333333333332), result)
     }
 
 }
