47963a49731742c2fec3c4736cc8b0018ffd24d3
ldavidgomez <david.gomez@apiumhub.com>
2019-01-23 12:48:18 +0100

    change test to parameterized test

--- src/test/kotlin/PrimeFactorsTest.kt
+++ src/test/kotlin/PrimeFactorsTest.kt
@@ -1,12 +1,15 @@
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class PrimeFactorsTest {
 
     private val primeFactors = PrimeFactors()
 
-    @Test
-    fun should_return_prime_factor_when_receive_a_number() {
+    @ParameterizedTest
+    @CsvSource("2, [2]")
+    fun should_return_prime_factor_when_receive_a_number(number: Int, expected: ArrayList<Int>) {
         val result = primeFactors.callForAction(2)
         assertEquals(arrayListOf(2), result)
     }
