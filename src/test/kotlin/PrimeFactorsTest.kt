e6ece45c70675de801b8f8181091df952aeb73f0
David Gomez <david.gomez@apiumhub.com>
2019-01-25 09:35:06 +0100

    Refactor

--- src/test/kotlin/PrimeFactorsTest.kt
+++ src/test/kotlin/PrimeFactorsTest.kt
@@ -1,14 +1,11 @@
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
 
 
-
-    class PrimeFactorsTest {
+class PrimeFactorsTest {
 
     private val primeFactors = PrimeFactors()
 
