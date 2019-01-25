a1e1235acb1710be287099b0e80d0a80e951eca7
David Gomez <david.gomez@apiumhub.com>
2019-01-25 09:34:47 +0100

    Fix parameterized test with array list arguments

--- src/test/kotlin/PrimeFactorsTest.kt
+++ src/test/kotlin/PrimeFactorsTest.kt
@@ -1,16 +1,33 @@
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
-class PrimeFactorsTest {
+
+
+    class PrimeFactorsTest {
 
     private val primeFactors = PrimeFactors()
 
     @ParameterizedTest
-    @CsvSource("2, [2]")
+    @MethodSource("generateData")
     fun should_return_prime_factor_when_receive_a_number(number: Int, expected: ArrayList<Int>) {
         val result = primeFactors.callForAction(2)
-        assertEquals(arrayListOf(2), result)
+        assertEquals(expected, result)
     }
+
+    companion object {
+        @JvmStatic
+        fun generateData(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(2, arrayListOf(2)),
+                Arguments.of(3, arrayListOf(3)),
+                Arguments.of(4, arrayListOf(2, 2))
+            )
+        }
+    }
+
 }
