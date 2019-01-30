308433f02e3c3fb2c643e07291530f8fbc987123
ldavidgomez <david.gomez@apiumhub.com>
2019-01-30 09:33:19 +0100

    First green test

--- src/test/kotlin/TicTacToeTest.kt
+++ src/test/kotlin/TicTacToeTest.kt
@@ -1,3 +1,24 @@
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.awt.Point
 
-internal class TicTacToeTest
+internal class TicTacToeTest{
+
+    private val ticTacToe = TicTacToe()
+
+    @Test
+    fun should_return_result_with_empty_board_and_no_result_value(){
+        val expected = Match()
+
+        //given
+        val player = "X"
+        val point = Point(0,0)
+
+        //when
+        val result = ticTacToe.callForAction(player, point)
+
+        //then
+        assertEquals(expected, result)
+    }
+
+}
