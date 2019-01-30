2fd9f3a7d67c8fad476eb16b061f0fb53545ad0b
ldavidgomez <david.gomez@apiumhub.com>
2019-01-30 09:33:07 +0100

    Add equals and hashcode to allow equals

--- src/main/kotlin/TicTacToe.kt
+++ src/main/kotlin/TicTacToe.kt
@@ -1,15 +1,37 @@
+import java.awt.Point
+
 class TicTacToe {
 
-    fun callForAction(number: Int): Match {
+    fun callForAction(player: String, point: Point): Match {
 
         return Match()
     }
 }
 
-data class Match(val board: Array<String> = emptyArray(), val result: ResultEnum = ResultEnum.DRAW) {}
+data class Match(val board: Array<String> = emptyArray(), val result: ResultEnum = ResultEnum.NO_RESULT) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Match
+
+        if (!board.contentEquals(other.board)) return false
+        if (result != other.result) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result1 = board.contentHashCode()
+        result1 = 31 * result1 + result.hashCode()
+        return result1
+    }
+}
 
 enum class ResultEnum {
     WIN_X,
     WIN_O,
-    DRAW
+    DRAW,
+    NO_RESULT
 }
