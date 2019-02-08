ef0baba499df400bf6fd669db233a4533abaa904
ldavidgomez <david.gomez@apiumhub.com>
2019-02-08 09:25:40 +0100

    Refactor

--- src/main/kotlin/TicTacToe.kt
+++ src/main/kotlin/TicTacToe.kt
@@ -1,5 +1,16 @@
 import java.awt.Point
 
+private val winCombos = arrayOf(
+    arrayOf(Point(0,0), Point(1,0), Point(2,0)),
+    arrayOf(Point(0,1), Point(1,1), Point(2,1)),
+    arrayOf(Point(0,2), Point(1,2), Point(2,2)),
+    arrayOf(Point(0,0), Point(0,1), Point(0,2)),
+    arrayOf(Point(1,0), Point(1,1), Point(1,2)),
+    arrayOf(Point(2,0), Point(2,1), Point(2,2)),
+    arrayOf(Point(0,0), Point(1,1), Point(2,2)),
+    arrayOf(Point(2,0), Point(1,1), Point(0,2))
+)
+
 class TicTacToe {
 
     fun callForAction(player: String, point: Point, currentBoard: Array<Array<String>>): Match {
@@ -14,7 +25,7 @@ class TicTacToe {
         for (row in 0..2) {
             for (col in 0..2 ) {
                 val currentPoint = Point(row, col)
-                if(point.equals(currentPoint) && board[col][row] == "")
+                if(point == currentPoint && board[col][row] == "")
                     board[col][row] = player
             }
         }
@@ -24,36 +35,24 @@ class TicTacToe {
 
     private fun setResult(board: Array<Array<String>>): ResultEnum {
 
-        val winCombos = arrayOf(
-            arrayOf(Point(0,0), Point(1,0), Point(2,0)),
-            arrayOf(Point(0,1), Point(1,1), Point(2,1)),
-            arrayOf(Point(0,2), Point(1,2), Point(2,2)),
-            arrayOf(Point(0,0), Point(0,1), Point(0,2)),
-            arrayOf(Point(1,0), Point(1,1), Point(1,2)),
-            arrayOf(Point(2,0), Point(2,1), Point(2,2)),
-            arrayOf(Point(0,0), Point(1,1), Point(2,2)),
-            arrayOf(Point(2,0), Point(1,1), Point(0,2))
-        )
-
         for (row in winCombos) {
             val a = row[0]
             val b = row[1]
             val c = row[2]
-            if (!board[a.y][a.x].isNullOrEmpty()) {
-                if (board[a.y][a.x].equals(board[b.y][b.x])) {
-                    if (board[a.y][a.x].equals(board[c.y][c.x])) {
+            if (!board[a.y][a.x].isEmpty()) {
+                if (board[a.y][a.x] == board[b.y][b.x]) {
+                    if (board[a.y][a.x] == board[c.y][c.x]) {
                         return getWinner((board[a.y][a.x]))
                     }
                 }
             }
         }
 
-        for (row in winCombos) {
-            val a = row[0]
-            val b = row[1]
-            val c = row[2]
-            if (board[a.y][a.x].isNullOrEmpty()) {
-                return ResultEnum.NONE
+        for (row in 0..2) {
+            for (col in 0..2 ) {
+                if (board[col][row].isEmpty()) {
+                    return ResultEnum.NONE
+                }
             }
         }
 
